61f3ca04aa36888712e40058ee0b484961a748f8
Max-N89 <4work.maxn@gmail.com>
2021-08-20 17:53:54 +0300

    Add an example of handling events

--- src/index.js
+++ src/index.js
@@ -8,24 +8,29 @@ const sandboxContainer = document.createElement("main");
 sandboxContainer.id = "sandbox_container";
 document.body.append(sandboxContainer);
 
+
+// React official documentation
 import HelloMessage from "./sandbox/react-off-docs/react-home-page-examples/hello-message";
 import Timer from "./sandbox/react-off-docs/react-home-page-examples/timer";
 import TodoApp from "./sandbox/react-off-docs/react-home-page-examples/todo-app";
 import MarkdownEditor from "./sandbox/react-off-docs/react-home-page-examples/markdown-editor";
-
 import Clock from "./sandbox/react-off-docs/rendering-elements/clock";
+import ToggleButton from "./sandbox/react-off-docs/handling-events/toggle-button";
 
 ReactDOM.render(
     <Fragment>
-        <HelloMessage to="Max"/>
-        <Timer/>
-        <TodoApp/>
-        <MarkdownEditor/>
-        <section>
-                <Clock/>
-                <Clock/>
-                <Clock/>
-        </section>
+            <HelloMessage to="Max"/>
+            <Timer/>
+            <TodoApp/>
+            <MarkdownEditor/>
+            <section>
+                    <Clock/>
+                    <Clock/>
+                    <Clock/>
+            </section>
+            <section>
+                <ToggleButton/>
+            </section>
     </Fragment>,
     sandboxContainer
 );
